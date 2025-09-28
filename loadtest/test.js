bfbb02cde69abdb197f1a1f8e9e06192187b3944
Oliver Wahlen <owahlen@googlemail.com>
2025-09-28 11:29:18 +0200

    fixed jvm cgroup problem

--- loadtest/test.js
+++ loadtest/test.js
@@ -3,20 +3,9 @@ import {sleep} from "k6";
 
 export let options = {
     scenarios: {
-        ramp_up: {
-            executor: "ramping-arrival-rate",
-            startTime: "0s",
-            startRate: 1, // Start at 1 request per second
-            timeUnit: "1s",
-            preAllocatedVUs: 50,
-            maxVUs: 500,
-            stages: [
-                {duration: "1m", target: 400}, // Ramp up to 400 requests/s over 2 minutes
-            ],
-        },
         constant_load: {
             executor: "constant-arrival-rate",
-            startTime: "1m", // Starts after the ramp-up stage
+            startTime: "0m", // Starts after the ramp-up stage
             rate: 400, // Maintain 400 requests per second
             timeUnit: "1s",
             preAllocatedVUs: 100,
